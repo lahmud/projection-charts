5e8c3c5b108c4f682dba488aba3b8f6cfc9de8f0
Lahiru TM <ltmudalige@gmail.com>
2020-03-21 16:53:43 -0400

    Add default route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { formatCurrency } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,9 @@ import { formatCurrency } from '@angular/common';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(private router: Router){}
+
   ngOnInit() {
-    
+    this.router.navigate(['/chartjs']);
   }
 }
